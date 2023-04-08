21e71a3852ac58ef8d8d6e82aa2f3b3e3af9c1e8
Icestab <dengyue35@qq.com>
2023-04-08 13:54:30 +0800

    # On branch main # Changes to be committed: #	modified:   docs/.vitepress/cache/deps/_metadata.json #	modified:   docs/.vitepress/config.ts #	modified:   node_modules/.modules.yaml #	modified:   node_modules/.pnpm/lock.yaml #	modified:   package.json #	modified:   pnpm-lock.yaml #

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vitepress'
-
+import { SearchPlugin } from "vitepress-plugin-search";
 // https://vitepress.dev/reference/site-config
+
 export default defineConfig({
   title: "个人杂记",
   description: "A Nodes Site",
   lastUpdated: true,
+
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: '主页', link: '/' },
-      { text: '指南', link: '/markdown-examples' }
+      { text: '指南', link: '/guide/readme' }
     ],
 
     sidebar: [
@@ -32,9 +34,25 @@ export default defineConfig({
     socialLinks: [
       { icon: 'github', link: 'https://github.com/Icestab' }
     ],
+    docFooter: { prev: '上一页', next: '下一页' },
     footer: {
       message: 'Released under the MIT License.',
       copyright: 'Copyright © 2023-present flysch'
+    },
+    lastUpdatedText: '上次更新',
+    returnToTopLabel: '返回顶部',
+    darkModeSwitchLabel: '主题',
+    outlineTitle: '此页',
+
+
+  },
+  vite: {
+    plugins: [SearchPlugin({
+      previewLength: 62,
+      buttonLabel: "搜索",
+      placeholder: "搜索文档",
+      tokenize: "full"
     }
+    )]
   }
 })
